8b90e218dd808644e1cfd5433066c53af3f69d5d
krnveersharma <115925693+krnveersharma@users.noreply.github.com>
2024-07-07 14:16:42 +0530

    build error fix

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ const Footer = () => {
       <div className="flex mt-16 md:flex-row flex-col justify-between items-center">
         <p className="md:text-base text-sm md:font-normal font-light">Copyright © 2024 Karanveer Sharma </p>
         <div className="flex items-center md:gap-3 gap-6">{socialMedia.map((profile)=>(
-            <Link href={profile.link}><div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
+            <Link href={profile.link} key={profile.id}><div key={profile.id} className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
                 <img src={profile.img}  width={20} height={20}/>
             </div></Link>
         ))}</div>
