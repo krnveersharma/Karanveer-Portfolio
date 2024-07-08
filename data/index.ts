ba06632e13a3ffbf06ee40a081559eb84b7898a7
krnveersharma <115925693+krnveersharma@users.noreply.github.com>
2024-07-08 17:21:08 +0530

    Project changes

--- data/index.ts
+++ data/index.ts
@@ -88,20 +88,20 @@ export const navItems = [
     },
     {
       id: 3,
-      title: "AI Image SaaS - Canva Application",
-      des: "A REAL Software-as-a-Service app with AI features and a payments and credits system using the latest tech stack.",
-      img: "/p3.svg",
-      iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
-      link: "https://github.com/adrianhajdin/ai_saas_app",
+      title: "Mern_Service",
+      des: "WEbsite where you can connect with me forthe web development service",
+      img: "/Mern_service.png",
+      iconLists: ["/re.svg", "/express.svg", "/tail.svg", "/js.svg", "/mongo.svg"],
+      link: "https://github.com/krnveersharma/mern_service",
     },
-    {
-      id: 4,
-      title: "Animated Apple Iphone 3D Website",
-      des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
-      img: "/p4.svg",
-      iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
-      link: "https://github.com/adrianhajdin/iphone",
-    },
+    // {
+    //   id: 4,
+    //   title: "Animated Apple Iphone 3D Website",
+    //   des: "Recreated the Apple iPhone 15 Pro website, combining GSAP animations and Three.js 3D effects..",
+    //   img: "/p4.svg",
+    //   iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    //   link: "https://github.com/adrianhajdin/iphone",
+    // },
   ];
   
 
